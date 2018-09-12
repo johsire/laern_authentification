38c3f5ee9cd0897285e9d7053d2bc62044766134
Josiah Maina <josiah5115@gmail.com>
2018-09-12 14:09:28 -0600

    auth0 login

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const app = express();
 // Destructuring from process.env:
 let {
  SERVER_PORT,
- SECRET
+ SECRET,
+ REACT_APP_CLIENT_ID
 } = process.env;
 
 
@@ -25,8 +26,29 @@ app.use(session({
 
 
 // Endpoints:
+app.get('auth/callback', async (req, res) => {
+ // req.qury = { code: someValue } --> req.query.code
+
+ // post request for access token, send code in body of request
+ let payload = {
+  client_id: REACT_APP_CLIENT_ID,
+  client_secret: CLIENT_SECRET,
+  code: req.query.code,
+  grant_type: 'authorization code',
+  redirect_url: `http://${req.headers.host}/auth/callback`
+ }
+})
 
 
+// post request access token send code in body of request
+let resWithToken = await axios.post(`https://${REACT_APP_DOMAIN}/oauth/token`, payload).then(res => {
+ // get request for user data, send access token:
+ let resWithUserData = await axios.get(`https//${REACT_APP_DOMAIN}/userinfo?
+ access_token=${resWithToken.data.access_token}`)
+
+ res.session.user = resWithUserData.data;
+ res.redirect('/');
+});
 
 // Listen on a specific port:
 app.listen(SERVER_PORT, () => {
