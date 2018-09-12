8d1a831b6c767c6859e748c46496f4109d855628
Josiah Maina <josiah5115@gmail.com>
2018-09-12 11:42:37 -0600

    server setup

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = ('express');
+const session = require('express-session');
+const axios = require('axios');
+
+const app = express();
+
+// Destructuring from process.env:
+let {
+ SERVER_PORT
+} = process.env;
+
+
+// middleware:
+app.use(express.json());
+
+
+
+// Endpoints:
+
+
+
+// Listen on a specific port:
+app.listen(SERVER_PORT, () => {
+ console.log(`Listening on port: ${SERVER_PORT}`)
+});
